44cd7582c7f3c4d058c8b2cd843d458739e4bc41
파링 <me@paring.moe>
2025-09-27 15:06:43 +0900

    fix: electron

--- packages/xmcl/package.nix
+++ packages/xmcl/package.nix
@@ -1,7 +1,7 @@
 {
   stdenv,
   fetchurl,
-  electron_35,
+  electron,
   bash,
   makeDesktopItem,
 }:
@@ -22,7 +22,7 @@ in
 stdenv.mkDerivation {
   inherit src pname version;
   buildInputs = [
-    electron_35
+    electron
   ];
 
   installPhase = ''
@@ -31,7 +31,7 @@ stdenv.mkDerivation {
     cp resources/app.asar $out/etc/xmcl/app.asar
     mkdir -p $out/bin
     echo '#!${bash}/bin/bash' > $out/bin/xmcl
-    echo "${electron_35}/bin/electron $out/etc/xmcl/app.asar \$@" >> $out/bin/xmcl
+    echo "${electron}/bin/electron $out/etc/xmcl/app.asar \$@" >> $out/bin/xmcl
     cp ${desktopEntry}/share/applications/${pname}.desktop $out/share/applications/${pname}.desktop
     chmod +x $out/bin/xmcl
   '';
